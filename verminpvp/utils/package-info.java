a3e0e94353a665f6b6dea57b8877a63559b1eb74
Dangel <sid753855@gmail.com>
2026-02-23 23:52:21 +0900

    Delete verminpvp directory

--- verminpvp/utils/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Utility classes for VerminPVP
- * 
- * This package contains utility classes for common operations:
- * - ItemProvider: Creates and manages class-specific items
- * - CooldownDisplay: Displays cooldown timers to players
- */
-package com.verminpvp.utils;
